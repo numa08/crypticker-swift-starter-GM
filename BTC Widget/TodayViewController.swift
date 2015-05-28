a8900e6ca387e2288a8de9372075eca9531aed09
Thomas Durand <thomas.durand.verjat@gmail.com>
2015-05-28 10:28:43 -0400

    Added Today's View following this tutorial : http://www.raywenderlich.com/83809/ios-8-today-extension-tutorial

--- BTC Widget/TodayViewController.swift
+++ BTC Widget/TodayViewController.swift
@@ -2,16 +2,16 @@
 //  TodayViewController.swift
 //  BTC Widget
 //
-//  Created by numanuma08 on 2014/10/17.
-//  Copyright (c) 2014年 Ray Wenderlich. All rights reserved.
+//  Created by Thomas Durand on 28/05/2015.
+//  Copyright (c) 2015 Ray Wenderlich. All rights reserved.
 //
 
 import UIKit
-import NotificationCenter
 import CryptoCurrencyKit
+import NotificationCenter
 
 class TodayViewController: CurrencyDataViewController, NCWidgetProviding {
-
+        
     @IBOutlet weak var toggleLineChartButton: UIButton!
     @IBOutlet weak var lineChartHeightConstraint: NSLayoutConstraint!
     
@@ -19,25 +19,45 @@ class TodayViewController: CurrencyDataViewController, NCWidgetProviding {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view from its nib.
         lineChartHeightConstraint.constant = 0
         
-        lineChartView.dataSource = self
         lineChartView.delegate = self
+        lineChartView.dataSource = self
         
         priceLabel.text = "--"
         priceChangeLabel.text = "--"
     }
     
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         
-        fetchPrices{ error in
+        fetchPrices { error in
             if error == nil {
                 self.updatePriceLabel()
                 self.updatePriceChangeLabel()
                 self.updatePriceHistoryLineChart()
             }
         }
+    }
+    
+    func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets {
+        return UIEdgeInsetsZero
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        // Perform any setup necessary in order to update the view.
+
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+
+        completionHandler(NCUpdateResult.NewData)
     }
     
     override func viewDidLayoutSubviews() {
@@ -51,32 +71,15 @@ class TodayViewController: CurrencyDataViewController, NCWidgetProviding {
                 blue: 0.82, alpha: 1.0)
     }
     
-    func widgetMarginInsetsForProposedMarginInsets(defaultMarginInsets: UIEdgeInsets) -> UIEdgeInsets {
-        return UIEdgeInsetsZero
-    }
-    
-    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
-        fetchPrices{ error in
-            if error == nil {
-                self.updatePriceLabel()
-                self.updatePriceChangeLabel()
-                self.updatePriceHistoryLineChart()
-                completionHandler(.NewData)
-            } else {
-                completionHandler(.NoData)
-            }
-        }
-    }
-    
     @IBAction func toggleLineChart(sender: UIButton) {
         if lineChartIsVisible {
             lineChartHeightConstraint.constant = 0
-            let transform = CGAffineTransformMakeRotation(CGFloat(0))
+            let transform = CGAffineTransformMakeRotation(0)
             toggleLineChartButton.transform = transform
             lineChartIsVisible = false
         } else {
             lineChartHeightConstraint.constant = 98
-            let transform = CGAffineTransformMakeRotation(CGFloat(180.0 * M_PI / 180.0))
+            let transform = CGAffineTransformMakeRotation(CGFloat(180.0 * M_PI/180.0))
             toggleLineChartButton.transform = transform
             lineChartIsVisible = true
         }
